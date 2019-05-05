941649ca94fef77e54b3637813de4037aa7410f8
vivekssm <37876104+vivekssm@users.noreply.github.com>
2019-05-05 20:46:08 +0530

    Add files via upload

---
+++ completeprojectravi.py
@@ -0,0 +1,73 @@
+# use natural language toolkit
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+# word stemmer
+stemmer = LancasterStemmer()
+import pandas as pd
+sample_data=[]
+y=[]
+file = open('dataset.txt', encoding="utf8")
+for a in file:
+    sample_data.append(a.split("\t")[0])
+    y.append(a.split("\t")[1])
+
+
+training_data = []    
+for a in range(len(sample_data)):
+    training_data.append({"class":y[a][:-1], "sentence":sample_data[a]})   
+    
+corpus_words = {}
+class_words = {}
+# turn a list into a set (of unique items) and then a list again (this removes duplicates)
+classes = list(set([a['class'] for a in training_data]))
+for c in classes:
+    # prepare a list of words within each class
+    class_words[c] = []
+
+# loop through each sentence in our training data
+for data in training_data:
+    # tokenize each sentence into words
+    for word in nltk.word_tokenize(data['sentence']):
+        # ignore a some things
+        if word not in ["?", "'s"]:
+            # stem and lowercase each word
+            stemmed_word = stemmer.stem(word.lower())
+            if stemmed_word not in corpus_words:
+                corpus_words[stemmed_word] = 1
+            else:
+                corpus_words[stemmed_word] += 1
+
+            # add the word to our words in class list
+            class_words[data['class']].extend([stemmed_word])
+
+
+def calculate_class_score_commonality(sentence, class_name, show_details=True):
+    score = 0
+    # tokenize each word in our new sentence
+    stop=['a','about','above','after','again','against','all','am','an','and','any','are','aren\'t','as','at','be','because','been','before','being','below','between','both','but','by','can\'t','cannot','could','couldn\'t','did','didn\'t','do','does','doesn\'t','doing','don\'t','down','during','each','few','for','from','further','had','hadn\'t','has','hasn\'t','have','haven\'t','having','he','he\'d','he\'ll','he\'s','her','here','here\'s','hers','herself','him','himself','his','how','how\'s','i','i\'d','i\'ll','i\'m','i\'ve','if','in','into','is','isn\'t','it','it\'s','its','itself','let\'s','me','more','most','mustn\'t','my','myself','no','nor','not','of','off','on','once','only','or','other','ought','our','ourselves','out','over','own','same','shan\'t','she','she\'d','she\'ll','she\'s','should','shouldn\'t','so','some','such','than','that','that\'s','the','their','theirs','them','themselves','then','there','there\'s','these','they','they\'d','they\'ll','they\'re','they\'ve','this','those','through','to','too','under','until','up','very','was','wasn\'t','we','I','Was',',','.','!','He','She','we\'d','we\'ll','we\'re','we\'ve','were','weren\'t','what','what\'s','when','when\'s','where','where\'s','#','@','$','com','%','&','?','which','while','who','who\'s','whom','why','why\'s','with','won\'t','would','wouldn\'t','you','you\'d','you\'ll','you\'re','you\'ve','your','yours','yourself','yourselves']
+    for word in nltk.word_tokenize(sentence):
+        # check to see if the stem of the word is in any of our classes
+        if stemmer.stem(word.lower()) in class_words[class_name]:
+            # treat each word with relative w
+            if(stemmer.stem(word.lower()) in stop ):
+                score += (1 / corpus_words[stemmer.stem(word.lower())])
+            else:
+                score += (1 + corpus_words[stemmer.stem(word.lower())])
+            if show_details:
+                print ("   match: %s (%s)" % (stemmer.stem(word.lower()), 1 / corpus_words[stemmer.stem(word.lower())]))
+    return score
+def classify(sentence):
+    high_class = None
+    high_score = 0
+    # loop through our classes
+    for c in class_words.keys():
+        # calculate score of sentence for each class
+        score = calculate_class_score_commonality(sentence, c, show_details=False)
+        # keep track of highest score
+        if score > high_score:
+            high_class = c
+            high_score = score
+
+    return high_class
+classify(input())
